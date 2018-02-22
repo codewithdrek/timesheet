bf72d43764e53569b205ea6dd985a19077046145
Ashish Gupta <ashish.gupta@autodata.net>
2018-02-22 12:53:41 +0530

    name and role done

--- src/main/java/com/supra/sso/controller/TimeSheetController.java
+++ src/main/java/com/supra/sso/controller/TimeSheetController.java
@@ -1,13 +1,17 @@
 package com.supra.sso.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TimeSheetController {
 
     @RequestMapping(value="/welcometimesheet")
-    public String openPageForModule() {
+    public String openPageForModule(@RequestParam("username") String username, @RequestParam(name="role") String roles, Model m) {
+    	m.addAttribute("username", username);
+    	m.addAttribute("role", roles.substring(1, roles.length()-1));
     	return "welcometimesheet";
     }
     
--- src/main/webapp/welcometimesheet.jsp
+++ src/main/webapp/welcometimesheet.jsp
@@ -1,6 +1,9 @@
 
 <%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>
 
-<h1>Hello <sec:authentication property="principal.username" /></h1><h3>This is timesheet module welcome page.</h3>
+<h1>Hello ${username}</h1>
+<h3>You are having role ${role}</h3>
+
+<h3>This is timesheet module welcome page.</h3>
 
 <h4>To Go back from this module </h4> <h2><a href="http://localhost:8080/sso/welcome">Click here</a></h2>
