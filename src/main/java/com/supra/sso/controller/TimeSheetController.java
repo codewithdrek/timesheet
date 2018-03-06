06f61ca0c0e60c3aa1d275d8e5d65f4e3d65e069
Ashish Gupta <ashish.gupta@autodata.net>
2018-03-06 16:56:37 +0530

    changes

--- src/main/java/com/supra/sso/controller/TimeSheetController.java
+++ src/main/java/com/supra/sso/controller/TimeSheetController.java
@@ -3,13 +3,17 @@ package com.supra.sso.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.supra.sso.model.User;
+import com.supra.sso.service.SecurityService;
 
 @Controller
 public class TimeSheetController {
@@ -30,4 +34,14 @@ public class TimeSheetController {
 			return "loginButton";
 		}
 	}
+	
+	@Autowired
+    SecurityService securityService;
+    
+    @RequestMapping(value="autologintimesheet")
+    @ResponseBody
+    public String autoLogin(@RequestParam("username") String username, @RequestParam("password") String password) {
+    	securityService.autologin(password, password);
+    	return "LOGGED IN TIMESHEET";
+    }
 }
